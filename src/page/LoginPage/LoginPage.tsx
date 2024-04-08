c28578e236e580f628ea54cba026f7f43dc22b23
Alexander Korobov <dktwho@mail.ru>
2024-04-08 12:04:50 +0300

    feat: used Input in LoginPage

--- src/page/LoginPage/LoginPage.tsx
+++ src/page/LoginPage/LoginPage.tsx
@@ -1,8 +1,11 @@
+import {useState} from 'react';
 import './LoginPage.css';
-import {Button} from "../../common/buttons";
+import {Input} from '../../common/fields';
+import {Button} from '../../common/buttons';
+
 
 export const LoginPage = () => {
-
+    const [formValues, setFormValues] = useState({username: '', password: ''});
     return (
         <div className='loginPage'>
             <div className='loginPage_container'>
@@ -11,12 +14,21 @@ export const LoginPage = () => {
 
 
                     <div className='loginPage_input_container'>
-
+                        <Input value={formValues.username} placeholder={'username'} isError={true}
+                               helperText={'validation'}
+                               onChange={(event) => setFormValues({
+                                   ...formValues,
+                                   username: event.currentTarget.value
+                               })}/>
 
                     </div>
 
                     <div className='loginPage_input_container'>
-
+                        <Input value={formValues.password} placeholder={'password'}
+                               onChange={(event) => setFormValues({
+                                   ...formValues,
+                                   password: event.currentTarget.value
+                               })}/>
                     </div>
 
                     <div>
