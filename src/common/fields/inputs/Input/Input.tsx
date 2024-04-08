8a9563fcce572be34eed45e2a053cfb216ae3319
Alexander Korobov <dktwho@mail.ru>
2024-04-08 12:03:55 +0300

    feat: create polymorph Input with module.css

---
+++ src/common/fields/inputs/Input/Input.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import  s from './Input.module.css';
+
+interface InputPropsType extends React.HTMLProps<HTMLInputElement> {
+    isError?: boolean;
+    helperText?: string;
+}
+
+export const Input: React.FC<InputPropsType> = ({ isError = false, helperText, ...props }) => {
+
+
+
+    return (
+        <>
+            <input className={`${s.input} ${isError ? s.error : ''}`} {...props} />
+            {isError && helperText && <div className={s.helper_text}>{helperText}</div>}
+        </>
+
+    );
+};
