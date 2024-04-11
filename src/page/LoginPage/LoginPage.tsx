0b624affb02663491e9e9589da7e105193f17091
Alexander Korobov <dktwho@mail.ru>
2024-04-11 18:38:23 +0300

    feat: perform LoginPage.css to module

--- src/page/LoginPage/LoginPage.tsx
+++ src/page/LoginPage/LoginPage.tsx
@@ -1,17 +1,18 @@
 import {useState} from 'react';
-import './LoginPage.css';
+// import './LoginPage.module.css';
+import s from './LoginPage.module.css'
 import {Input} from '../../common/fields';
 import {Button} from "../../common/buttons";
 
 export const LoginPage = () => {
     const [formValues, setFormValues] = useState({username: '', password: ''});
     return (
-        <div className='loginPage'>
-            <div className='loginPage_container'>
+        <div className={s.loginPage}>
+            <div className={s.container}>
                 <div>header</div>
-                <div className='loginPage_form_container'>
+                <div className={s.form_container}>
 
-                    <div className='loginPage_input_container'>
+                    <div className={s.input_container}>
                         <Input value={formValues.username} placeholder={'username'} isError={true}
                                helperText={'validation'}
                                onChange={(event) => setFormValues({
@@ -21,7 +22,7 @@ export const LoginPage = () => {
 
                     </div>
 
-                    <div className='loginPage_input_container'>
+                    <div className={s.input_container}>
                         <Input value={formValues.password} placeholder={'password'}
                                onChange={(event) => setFormValues({
                                    ...formValues,
